2abcee85da03ba0b4842ca4760eb6a5292016dc9
ram3917 <ramashwin.kumar@gmail.com>
2022-07-25 12:17:53 +0200

    First implementation of a Flask APP

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Data Predictor
 
-Webpage viewer to display plots
+Webpage viewer to display plots of Stock prices over time on a Web-Page
 
+<img src=Example.png>
 
 Packages to install:
 * yfinance
+* plotly
+* Flask
+
+For requirements look into : requirements.txt
---
+++ flask_app/app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template
+
+import plotly
+import plotly.graph_objs as go
+
+import pandas as pd
+import numpy as np
+import json
+
+import DataCollector as dc
+
+app = Flask(__name__, static_folder="./static", template_folder="./static")
+
+
+def create_graph():
+    """
+    The following script downloads and plots data from Stock market.
+    """
+    cols = ["Tata", "Reliance"]
+    df = pd.DataFrame(data=None,columns=cols)
+
+    # Get closing price
+    tata_df = dc.GetStockData("TATAMOTORS.NS")
+    df["Tata"] = tata_df['Close'].copy()
+
+    reliance_df = dc.GetStockData("RELIANCE.NS")
+    df["Reliance"] = reliance_df['Close'].copy()
+
+    # drop na
+    df.fillna(method='ffill', inplace=True)
+    df.fillna(method='bfill', inplace=True)
+
+    data = [
+        go.Line(
+            x=df.index, # assign x as the dataframe column 'x'
+            y=df['Tata']
+        ),
+        go.Line(
+            x=df.index, # assign x as the dataframe column 'x'
+            y=df['Reliance']
+        )
+    ]
+
+    # JSON for plotting
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON
+
+
+@app.route('/')
+def index():
+    graph = create_graph()
+    return render_template("index.html", plot=graph)
+
+
+if __name__ == '__main__':
+    app.run()
