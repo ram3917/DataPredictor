689b5905aaf10319837f9aac10c0a35030db59c7
ram3917 <ramashwin.kumar@gmail.com>
2023-04-30 10:54:11 +0200

    First commit streamlit app

---
+++ streamlit_app/app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+from data_collector import *
+
+st.title('Stock Price for the Last Year')
+
+# Using object notation
+select_exchange = st.sidebar.selectbox(
+    "Which Stock do you want to predict?",
+    (stockList['Exchange'].unique())
+)
+
+if select_exchange:
+    # Using object notation
+    select_name = st.sidebar.selectbox(
+        "Which Stock do you want to predict?",
+        (stockList[stockList['Exchange']==select_exchange]['Name'].to_list())
+    )
+else:
+    # Using object notation
+    select_name = st.sidebar.selectbox(
+        "Which Stock do you want to predict?",
+        (stockList['Name'].to_list())
+    )
+
+if select_exchange and select_name:
+    
+    symbol = stockList.loc[(stockList['Exchange']==select_exchange) & (stockList['Name']==select_name)]
+    symbol = symbol.reset_index()
+        
+    # Get closing price
+    df = GetStockData(symbol['Ticker'][0])
+    df = df.reset_index()
+    # drop na
+    df.fillna(method='ffill', inplace=True)
+    df.fillna(method='bfill', inplace=True)
+
+    # Calculate the rolling averages for 7 days and 30 days
+    df['Weekly_average'] = df['Close'].rolling(7).mean()
+    df['Monthly_average'] = df['Close'].rolling(30).mean()
+
+    plot_df = df[['Date','Close', 'Weekly_average','Monthly_average']].copy()
+
+    # st.header(symbol)
+    st.line_chart(plot_df, x='Date', y=['Close', 'Monthly_average'])
---
+++ streamlit_app/data_collector.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 20:41:43 2021
+
+@author: Rama Mallavarapu
+"""
+import os
+import pandas as pd
+import yfinance as yf
+
+"""
+DATA-COLLECTOR shall gather data for selected stocks from yahoo finance.
+"""
+
+'''
+Source : https://investexcel.net/all-yahoo-finance-stock-tickers/
+'''
+stockList = pd.read_excel(io="./flask_app/static/Yahoo Ticker Symbols - September 2017.xlsx",
+                sheet_name= 'Stock', usecols='A:E', skiprows=3, index_col=None)
+
+stockList.reset_index(drop=True)
+
+def GetStockData(index="", duration="max"):
+    # Query data
+    idx = yf.Ticker(index)
+    df = idx.history(period=duration, interval="1d")
+    
+    return df
+
+
+def CalculateAverageReturns(index=""):
+    pass
+
+def CalculateChange(index=""):
+    pass
