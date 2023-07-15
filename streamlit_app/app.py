fe3a2269953d3af9e5f361b013bd20a5e8393944
ram3917 <ramashwin.kumar@gmail.com>
2023-07-15 23:19:44 +0200

    Udpate to add deltas

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,6 +1,18 @@
 import streamlit as st
-import pandas as pd
+import plotly.express as px
+import plotly.figure_factory as ff
 from data_collector import *
+import streamlit.components.v1 as components
+
+# Set config
+def set_page_config():
+    st.set_page_config(
+        page_title="Finance Dashboard",
+        page_icon=":line_chart:",
+        layout="wide",
+        initial_sidebar_state="expanded",
+    )
+    st.markdown("<style> footer {visibility: hidden;} </style>", unsafe_allow_html=True)
 
 st.title('Stock Price for the Last Year')
 
@@ -41,5 +53,23 @@ if select_exchange and select_name:
 
     plot_df = df[['Date','Close', 'Weekly_average','Monthly_average']].copy()
 
+    # Set title    
     st.header(select_name)
-    st.line_chart(plot_df, x='Date', y=['Close', 'Monthly_average'])
+    # Line chart
+    st.line_chart(df, x='Date', y=['Close', 'Weekly_average', 'Monthly_average'])
+    
+    # Add columns to show changes
+    col1, col2, col3 = st.columns(3)
+    # Show 3 months change
+    currVal, deltaVal = CalculateThreeMonthHigh(df, 3)
+    col1.metric(label="Delta 3 months", value="%f EUR" % (currVal), delta="%f EUR" % (deltaVal))
+    
+    # Show 3 months change
+    currVal, deltaVal = CalculateThreeMonthHigh(df, 6)
+    col2.metric(label="Delta 6 months", value="%f EUR" % (currVal), delta="%f EUR" % (deltaVal))
+
+    # Show 3 months change
+    currVal, deltaVal = CalculateThreeMonthHigh(df, 12)
+    col3.metric(label="Delta 12 months", value="%f EUR" % (currVal), delta="%f EUR" % (deltaVal))
+
+    components.html('<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9719516206957584" crossorigin="anonymous"></script>', height=300)
--- streamlit_app/data_collector.py
+++ streamlit_app/data_collector.py
@@ -5,32 +5,52 @@ Created on Mon Feb  1 20:41:43 2021
 
 @author: Rama Mallavarapu
 """
-import os
+from forex_python.converter import CurrencyRates
 import pandas as pd
+from pandas.tseries.offsets import DateOffset
 import yfinance as yf
 
 """
 DATA-COLLECTOR shall gather data for selected stocks from yahoo finance.
 """
 
-'''
-Source : https://investexcel.net/all-yahoo-finance-stock-tickers/
-'''
-stockList = pd.read_excel(io="./streamlit_app/Yahoo Ticker Symbols - September 2017.xlsx",
-                sheet_name= 'Stock', usecols='A:E', skiprows=3, index_col=None)
+# '''
+# Source : https://investexcel.net/all-yahoo-finance-stock-tickers/
+# '''
+# stockList = pd.read_excel(io="./streamlit_app/Yahoo Ticker Symbols - September 2017.xlsx",
+#                 sheet_name= 'Stock', usecols='A:E', skiprows=3, index_col=None)
 
+stockList = pd.read_csv("./streamlit_app/MyPortfolio.csv", sep=';')
 stockList.reset_index(drop=True)
 
 def GetStockData(index="", duration="max"):
     # Query data
     idx = yf.Ticker(index)
     df = idx.history(period=duration, interval="1d")
-    
+
+    # Convert the rates to Euros - I trade in EUROS
+    c = CurrencyRates()
+    exchange_value = c.get_rate('USD', 'EUR')
+
+    # df['Open'] = df['Open'] * exchange_value
+    # df['High'] = df['High'] * exchange_value
+    # df['Low'] = df['Low'] * exchange_value
+    df['Close'] = df['Close'] * exchange_value
+
+    df = df['Close'].round(2)
+
     return df
 
+def CalculateThreeMonthHigh(df, offsetMonths=3):
+    currDate  = df['Date'][-1:]
+    startDate = currDate - DateOffset(months=offsetMonths)
 
-def CalculateAverageReturns(index=""):
-    pass
+    df = df[df['Date'].between(startDate.to_list()[0], currDate.to_list()[0])]
+
+    maxVal = df['Close'].max()
+    currVal = df['Close'][-1:]
+
+    return maxVal, (currVal - maxVal)
 
 def CalculateChange(index=""):
     pass
