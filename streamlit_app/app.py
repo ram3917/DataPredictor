c3ff4d44ee5dc9f897be8e73aa9302469894ad70
ram3917 <ramashwin.kumar@gmail.com>
2023-07-16 12:26:08 +0200

    Update app function

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -14,6 +14,29 @@ def set_page_config():
     )
     st.markdown("<style> footer {visibility: hidden;} </style>", unsafe_allow_html=True)
 
+def plot_chart(exchange, stock):
+    symbol = stockList.loc[(stockList['Exchange']==exchange) & (stockList['Name']==stock)]
+    symbol = symbol.reset_index()
+        
+    # Get closing price
+    df = GetStockData(symbol['Ticker'][0], '5y')
+    df = df.reset_index()
+    # drop na
+    df.fillna(method='ffill', inplace=True)
+    df.fillna(method='bfill', inplace=True)
+
+    # Calculate the rolling averages for 7 days and 30 days
+    df['Weekly_average'] = df['Close'].rolling(7).mean()
+    df['Monthly_average'] = df['Close'].rolling(30).mean()
+
+    plot_df = df[['Date','Close', 'Weekly_average','Monthly_average']].copy()
+
+    # Set title    
+    st.header(select_name)
+    # Line chart
+    st.line_chart(df, x='Date', y=['Close', 'Weekly_average', 'Monthly_average'])
+
+
 st.title('Stock Price for the Last Year')
 
 # Using object notation
@@ -53,26 +76,3 @@ if select_exchange and select_name:
                      value="{:.2f} EUR".format(maxVal), delta="{0:.2f} %".format(deltaChange))   
 
 components.html('<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="ramashwinkX" data-color="#FF5F5F" data-emoji="☕"  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-position="Right" data-font-color="#ffffff" data-coffee-color="#FFDD00" ></script>')
-
-
-def plot_chart(exchange, stock):
-    symbol = stockList.loc[(stockList['Exchange']==exchange) & (stockList['Name']==stock)]
-    symbol = symbol.reset_index()
-        
-    # Get closing price
-    df = GetStockData(symbol['Ticker'][0], '5y')
-    df = df.reset_index()
-    # drop na
-    df.fillna(method='ffill', inplace=True)
-    df.fillna(method='bfill', inplace=True)
-
-    # Calculate the rolling averages for 7 days and 30 days
-    df['Weekly_average'] = df['Close'].rolling(7).mean()
-    df['Monthly_average'] = df['Close'].rolling(30).mean()
-
-    plot_df = df[['Date','Close', 'Weekly_average','Monthly_average']].copy()
-
-    # Set title    
-    st.header(select_name)
-    # Line chart
-    st.line_chart(df, x='Date', y=['Close', 'Weekly_average', 'Monthly_average'])
